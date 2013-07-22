ac65cae8edab3c895b1534d617f4a52bb9d3d593
Birk Nilson <birk@tictail.com>
2013-07-22 16:31:06 +0200

    Syntax highlighting with Solarized256 theme

--- scripts/generate_website.py
+++ scripts/generate_website.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
 import os
+
 import markdown2
+
+from solarized256 import Solarized256Style
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
 
 
 def rel(path):
@@ -16,13 +22,54 @@ def read(filename):
     return contents
 
 
+def python_highlight(output, offset=0):
+    start_needle = '```python'
+    highlight_offset = output.find(start_needle, offset)
+    if highlight_offset == -1:
+        return (False, output)
+
+
+    end_needle = '```'
+    highlight_finish = output.find(end_needle,
+                                   highlight_offset + len(start_needle))
+
+    if highlight_finish == -1:
+        return (False, output)
+
+    formatter = HtmlFormatter(style=Solarized256Style)
+    code = output[highlight_offset + len(start_needle):highlight_finish]
+    code = highlight(code, PythonLexer(), formatter)
+
+    highlight_finish += len(end_needle)
+    highlighted = '{before}{code}{after}'.format(
+        before=output[:highlight_offset],
+        code=code,
+        after=output[highlight_finish:],
+    )
+    highlight_offset += len(start_needle)
+    return (highlight_finish, highlighted)
+
+
+def syntax_highlight(output):
+    highlight_offset = 0
+    while True:
+        highlight_offset, output = python_highlight(output,
+                                                    offset=highlight_offset)
+        if not highlight_offset:
+            break
+
+    return output
+
+
 def inject_markdown(template, md):
-    md = markdown2.markdown(md)
     return template.replace('{{{MARKDOWN}}}', md)
 
 
 def main():
     readme = read(rel('../README.md'))
+    readme = syntax_highlight(readme)
+    readme = markdown2.markdown(readme)
+
     template = read(rel('data/website_template.html'))
     output = inject_markdown(template, readme)
     with open(rel('../website/index.html'), 'w') as f:
